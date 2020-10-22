fd59085d77160d81bb40a6e8b392f9319dfc4cdd
John Guo <jg852@cornell.edu>
2020-10-22 03:05:15 -0400

    complete training part 1

--- emobank_sentiment_analysis_nltk.py
+++ emobank_sentiment_analysis_nltk.py
@@ -8,8 +8,65 @@ This script
 3. Allows the user to try putting in their own sentences for the classifier. 
 """
 
+from preprocessing_utils import closest_emotion, preprocess_text, EmotionVAD
+from nltk import FreqDist, classify, NaiveBayesClassifier, MaxentClassifier
+
 import pandas as pd 
-from preprocessing_utils import closest_emotion
+import random
+
+MAX_ENT_MAX_ITER = 10
+NUM_DATA_PARTITIONS = 10
+
+
+def preprocess_dataset(emo_df): 
+    """
+    Preprocesses the Pandas Dataframe for EmoBank. This function: 
+    
+    1. Preprocesses + Tokenizes input text.
+    2. Converts VAD values to their closest basic emotion. 
+    """
+    dataset = []
+    for ind, row in emo_df.iterrows(): 
+        emotion_vad = _normalize_emobank_VAD(EmotionVAD(row['V'], row['A'], row['D']))
+        emotion_label = closest_emotion(emotion_vad)
+
+        text_tokens = preprocess_text(row['text'])
+
+        datapoint = (text_tokens, emotion_label)
+        dataset.append(datapoint)
+
+    return dataset 
+
+
+def k_fold_cross_validation(dataset, k, classifier_type="naive_bayes"):
+    """ 
+    Creates a classifier via K-Fold cross validation. 
+    """ 
+    random.shuffle(dataset)
+    max_accuracy = 0.0
+    best_classifier = None
+
+    partition_size = len(dataset) // k
+
+    for i in range(k): 
+        test_start = i*partition_size
+        test_end = (i+1)*partition_size
+
+        test_data = dataset[test_start:test_end]
+        train_data = dataset[:test_start] + dataset[test_end:]
+
+        if classifier_type == "naive_bayes": 
+            classifier = NaiveBayesClassifier.train(train_data)
+        else: 
+            classifier = MaxentClassifier.train(train_data, max_iter=MAX_ENT_MAX_ITER)
+        accuracy = classify.accuracy(classifier, test_data)
+
+        if accuracy > max_accuracy: 
+            best_classifier = classifier
+            max_accuracy = accuracy
+
+    print(max_accuracy)
+    return best_classifier
 
 
 def _normalize_emobank_VAD(emotion_vad): 
@@ -17,32 +74,24 @@ def _normalize_emobank_VAD(emotion_vad):
     Normalized EmoBank VAD values (in range [1,5]) to be in [-1, 1]. 
     """
     normalize = lambda x : (x-3)/2
-    return map(normalize, emotion_vad)
-
-
-def preprocess_dataset(emo_df): 
-    """
-    Preprocesses the Pandas Dataframe for EmoBank from 
-
-
-    """
-    dataset = []
-    for ind, row in emo_df.iterrows(): 
-        emotion_vad = _normalize_emobank_VAD(Emotion(row['V'], row['A'], row['D']))
-        emotion_label = closest_emotion(vad_label)
-
-        # preprocess text 
-
-        datapoint = (row['text'], emotion_label)
-        dataset.append(datapoint)
+    return EmotionVAD(
+        normalize(emotion_vad.V), 
+        normalize(emotion_vad.A),
+        normalize(emotion_vad.D),
+    )    
 
 
 if __name__ == "__main__": 
-    print("Preprocessing EmoBank data...")
+    print("Loading EmoBank data...")
     emo_df = pd.read_csv("EmoBank/corpus/emobank.csv", index_col=0)
+
     print("Preprocessing EmoBank data...")
-    print("Training a Naive Bayes classifier...")
+    dataset = preprocess_dataset(emo_df)
+
+    print("Training a model...")
+    classifier = k_fold_cross_validation(dataset, NUM_DATA_PARTITIONS)
 
     while True: 
-
+        sentence = input("Type a sentence to try classifying: ")
+        print(classifier.classify(preprocess_text(sentence)))
 
--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -3,24 +3,40 @@
 Contains a suite of utility functions for preprocessing NLP data and VAD emotion results. 
 """
 
-import math
 from collections import namedtuple
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag, NaiveBayesClassifier
+
+import re, math
 
 
 EmotionVAD = namedtuple('Emotion', ['V', 'A', 'D'])
 # V, A and D represent Valence (negative vs. positive), 
 # Arousal (calm vs. excited), and Dominance (being controlled vs. being in control)
 
-# Ekman's Six Basic Emotions and their VAD values
-ANGER = EmotionVAD(-0.43, 0.67, 0.34)
-JOY = EmotionVAD(0.76, 0.48, 0.35)
-SURPRISE = EmotionVAD(0.4, 0.67, -0.13)
-DISGUST = EmotionVAD(-0.6, 0.35, 0.11)
-FEAR = EmotionVAD(-0.64, 0.6, -0.43)
-SADNESS = EmotionVAD(-0.63, 0.27, -0.33)
+# 8 emotions at the extreme of the cube
+PROTECTED = EmotionVAD(1, -1, -1)
+SATISFIED = EmotionVAD(1, -1, 1)
+SURPRISED = EmotionVAD(1, 1, -1)
+JOYFUL = EmotionVAD(1 ,1, 1)
+SAD = EmotionVAD(-1, -1, -1)
+UNCONCERNED = EmotionVAD(-1, -1, 1)
+FEAR = EmotionVAD(-1, 1, -1)
+ANGRY = EmotionVAD(-1, 1, 1)
 
-BASIC_EMOTION_STRINGS = {ANGER: "Anger", JOY: "Joy", SURPRISE: "Surprise", DISGUST: "Disgust", 
-                        FEAR: "Fear", SADNESS: "Sadness"}
+
+BASIC_EMOTION_STRINGS = {
+    PROTECTED : "Protected", 
+    SATISFIED : "Satisfied", 
+    SURPRISED : "Surprised", 
+    JOYFUL : "Joyful", 
+    SAD : "Sad", 
+    UNCONCERNED : "Unconcerned", 
+    FEAR : "Fear", 
+    ANGRY : "Angry",
+}
 
 
 def closest_emotion(emotion_vad): 
@@ -29,7 +45,7 @@ def closest_emotion(emotion_vad):
         emotion_vad : EmotionVAD. The VAD values for the emotion to analyze, where each value
             has been normalized to be in the range [-1, 1].
     Returns: 
-        Which of the six basic Ekman emotions most resembles the VAD input values. 
+        Which of the basic emotions most resembles the VAD input values. 
     """
     min_val, min_emotion = 10, ""
     for basic_emotion in BASIC_EMOTION_STRINGS: 
@@ -41,13 +57,44 @@ def closest_emotion(emotion_vad):
     return BASIC_EMOTION_STRINGS[min_emotion]
 
 
+def preprocess_text(text): 
+    """
+    Preprocess the given text into tokens. This function
+        - Removes stop words
+        - Lemmatizes tokens
+        - POS Tagging 
+
+    Preconditions: 
+        text : String. The text to preprocess. 
+    Returns: 
+        A list of cleaned tokens.  
+    """
+    # Preprocess String
+    text = text.lower()
+    text = text.strip()
+    text = _remove_punctuation(text) 
+
+    # Tokenize 
+    tokens = word_tokenize(text)
+
+    # Remove Stopwords + Lemmatize forms
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    cleaned_tokens = {
+        lemmatizer.lemmatize(token) : True for token in tokens if not token in stop_words
+    }
+
+    return cleaned_tokens 
+
+
 def _norm_between_points(emotion_vad_1, emotion_vad_2): 
-    """ 
-    Returns: The Euclidean distance between two VAD triplets. 
-    """ 
     return math.sqrt(
         (emotion_vad_1.V - emotion_vad_2.V)**2 + 
         (emotion_vad_1.A - emotion_vad_2.A)**2 + 
         (emotion_vad_1.D - emotion_vad_2.D)**2
     )
 
+
+def _remove_punctuation(text): 
+    return re.sub(r"[^a-zA-Z0-9]+", ' ', text)
+    
